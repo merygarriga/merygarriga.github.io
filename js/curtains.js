502134f96de21c5c6ff5a966589f4fdd81041ab7
Maria <mariagarriga10@gmail.com>
2023-05-18 10:38:31 +0200

    timeoutCurtains + PHOTOS collage

--- js/curtains.js
+++ js/curtains.js
@@ -1,18 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.addEventListener('click', function(event) {
-      if (event.target.matches('.curtain')) {
-        var body = document.querySelector('body');
-        body.classList.toggle('open-curtains');
+document.addEventListener("DOMContentLoaded", function () {
+  var body = document.querySelector("body");
+  var curtain = true;
+  var timeout;
+
+  if (curtain) {
+    timeout = setTimeout(function() {
+      body.classList.toggle("open-curtains");
+    }, 2000);
+
+    document.addEventListener("click", function (event) {
+      if (event.target.matches(".curtain")) {
+        body.classList.toggle("open-curtains");
+        clearTimeout(timeout);
       }
     });
-  });
 
-  var cursor = document.createElement("div");
-  cursor.classList.add("cursor");
-  document.body.appendChild(cursor);
-  
-  document.addEventListener("mousemove", function(e) {
-    cursor.style.top = e.pageY + "px";
-    cursor.style.left = e.pageX + "px";
-  });
-  
+    curtain = false;
+  }
+});
+
+var cursor = document.createElement("div");
+cursor.classList.add("cursor");
+document.body.appendChild(cursor);
+
+document.addEventListener("mousemove", function (e) {
+  cursor.style.top = e.pageY + "px";
+  cursor.style.left = e.pageX + "px";
+});
